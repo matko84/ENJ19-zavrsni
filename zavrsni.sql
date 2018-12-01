0f347666f3c7911226879c95d2825626e6cab9f6
matko84 <>
2018-12-01 23:55:01 +0100

    Fresh start

--- zavrsni.sql
+++ zavrsni.sql
@@ -11,10 +11,7 @@ naziv_tvrtke				varchar(50)			not null,
 ulica						varchar(50)			not null,
 mjesto						varchar(50)			not null,
 postanski_broj				decimal(10,2)		not null,
-drzava						varchar(50)			not null,
-mobitel						varchar(50),
 telefon						varchar(50),
-faks						varchar(50),
 email						varchar(50)
 );
 
@@ -33,32 +30,31 @@ reg_oznaka_prikolice		varchar(50)
 create table STAVKA (
 sifra						int					not null		primary key			auto_increment,		
 redni_broj					int					not null,
-naziv_robe					varchar(50)			not null,
+proizvod					int					not null,
 jedinicna_mjera				varchar(50)			not null,
-kolicina					decimal(10,2)		not null
+skladisna_primka			int					not null,
+bruto						decimal(10,2)		not null,
+tara						decimal(10,2)		not null,
+netto						decimal(10,2)												
 );
 
 create table ISPORUCITELJ (
 sifra						int					not null		primary key			auto_increment,
 tvrtka						int					not null,
-broj_otpremnice				varchar(50)			not null,
-broj_narudzbenice			varchar(50),
-isporucio					int					not null
+isporucio					varchar(50)			not null
 );
 
 create table PRIJEVOZNIK (
 sifra						int					not null		primary key			auto_increment,
 tvrtka						int					not null,
 broj_prijevoznog_lista		varchar(50)			not null,
-vozac						int					not null,
+vozac						varchar(50)			not null,
 vozilo						int					not null
 );
 
 create table PRIMATELJ (
 sifra						int					not null		primary key			auto_increment,
-tvrtka						int					not null,
-robu_zaprimio				int					not null,
-vagar						int					not null
+skladiste					varchar(50)			not null
 );
 
 create table SKLADISNA_PRIMKA(
@@ -68,21 +64,28 @@ datum						datetime			not null,
 isporucitelj				int					not null,
 prijevoznik					int					not null,
 primatelj					int 				not null,
-stavka						int					not null
+broj_narudzbenice			varchar(50)			not null,
+broj_otpremnice				varchar(50)			not null,
+vagar						int					not null,
+robu_zaprimio				int					not null
 );
 
+create table PROIZVOD (
+sifra						int					not null		primary key			auto_increment,
+naziv_proizvoda				varchar(50)			not null,
+kategorija					varchar(50)			not null
+);
+
+alter table 	STAVKA		 	add foreign key (skladisna_primka)	references SKLADISNA_PRIMKA(sifra);
+alter table 	STAVKA		 	add foreign key (proizvod)			references PROIZVOD(sifra);
+
 alter table 	ISPORUCITELJ 	add foreign key (tvrtka) 			references TVRTKA(sifra);
-alter table 	ISPORUCITELJ 	add foreign key (isporucio) 		references OSOBA(sifra);
 
 alter table 	PRIJEVOZNIK 	add foreign key (tvrtka) 			references TVRTKA(sifra);
-alter table 	PRIJEVOZNIK 	add foreign key (vozac) 			references OSOBA(sifra);
 alter table 	PRIJEVOZNIK 	add foreign key (vozilo) 			references VOZILO(sifra);
 
-alter table 	PRIMATELJ 		add foreign key (tvrtka) 			references TVRTKA(sifra);
-alter table 	PRIMATELJ 		add foreign key (robu_zaprimio) 	references OSOBA(sifra);
-alter table 	PRIMATELJ 		add foreign key (vagar) 			references OSOBA(sifra);
-
+alter table 	SKLADISNA_PRIMKA add foreign key (robu_zaprimio) 	references OSOBA(sifra);
+alter table 	SKLADISNA_PRIMKA add foreign key (vagar) 			references OSOBA(sifra);
 alter table 	SKLADISNA_PRIMKA add foreign key (isporucitelj) 	references ISPORUCITELJ(sifra);
 alter table 	SKLADISNA_PRIMKA add foreign key (prijevoznik)	 	references PRIJEVOZNIK(sifra);
 alter table 	SKLADISNA_PRIMKA add foreign key (primatelj) 		references PRIMATELJ(sifra);
-alter table 	SKLADISNA_PRIMKA add foreign key (stavka) 			references STAVKA(sifra);
